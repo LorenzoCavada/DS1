efedb177a8c58d4c2e6e01ce77b411953df2f747
Alessandro Brighenti <alessandro.brighenti@studenti.unitn.it>
2022-06-27 16:47:30 +0200

    class for messages

---
+++ src/main/java/it/unitn/ds1/Messages.java
@@ -0,0 +1,63 @@
+package it.unitn.ds1;
+
+import akka.actor.ActorRef;
+
+import java.io.Serializable;
+import java.util.Stack;
+
+public class Messages {
+    public static class ReadReqMsg implements Serializable {
+        public final int key;   // key of requested item
+        public Stack<ActorRef> responsePath;
+
+        public ReadReqMsg(int key) {
+            this.key=key;
+            this.responsePath=new Stack<>();
+        }
+    }
+
+    public static class ReadRespMsg implements Serializable {
+        public final int key;   // key of requested item
+        public final int value; //value of requested item
+        public Stack<ActorRef> responsePath;
+
+        public ReadRespMsg(int key, int value, Stack<ActorRef> stack) {
+            this.key=key;
+            this.value=value;
+            this.responsePath= (Stack<ActorRef>) stack.clone();
+        }
+    }
+
+    public static class WriteReqMsg implements Serializable {
+        public final int key;   // key of item to be written
+        public final int newValue; //new value of item
+        public ActorRef originator; //originato of request
+
+        public WriteReqMsg(int key, int newValue, ActorRef originator) {
+            this.key=key;
+            this.newValue=newValue;
+            this.originator=originator;
+        }
+    }
+
+    public static class WriteConfirmMsg implements Serializable {
+        public final int key;   // key of written item
+
+        public WriteConfirmMsg(int key) {
+            this.key=key;
+        }
+    }
+
+    public static class RefillMsg implements Serializable {
+        public final int key;   // key of item to be written
+        public final int newValue; //new value of item
+        public ActorRef originator;
+
+        public RefillMsg(int key, int newValue, ActorRef originator) {
+            this.key=key;
+            this.newValue=newValue;
+            this.originator=originator;
+        }
+    }
+
+}
