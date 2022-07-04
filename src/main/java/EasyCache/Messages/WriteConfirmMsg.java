7dcc418eec92fcb7640b9efead185c5b9c3747cb
Alessandro Brighenti <alessandro.brighenti@studenti.unitn.it>
2022-07-04 17:23:00 +0200

    add uuid to write confirm

--- src/main/java/EasyCache/Messages/WriteConfirmMsg.java
+++ src/main/java/EasyCache/Messages/WriteConfirmMsg.java
@@ -1,12 +1,17 @@
 package EasyCache.Messages;
 
+import java.util.UUID;
+
 // Represent the confirmation of the write operation.
 // This will be originated by the L2 cache and sent to the client only if the L2 cache will see that the originator of the request is one of its children
 // This message is so originated after have received a RefillMsg and found the originator in the L2 cache's children list
 public class WriteConfirmMsg extends Message{
     public final int key;   // key of written item
 
-    public WriteConfirmMsg(int key) {
+    public final UUID uuid; //uuid of request
+
+    public WriteConfirmMsg(int key, UUID uuid) {
         this.key=key;
+        this.uuid=uuid;
     }
 }
