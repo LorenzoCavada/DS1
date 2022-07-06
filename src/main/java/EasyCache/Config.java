3c77539df536bec9504bf25d306956dc172d060c
Alessandro Brighenti <alessandro.brighenti@studenti.unitn.it>
2022-07-06 11:03:01 +0200

    added timeouts and recover parameters

--- src/main/java/EasyCache/Config.java
+++ src/main/java/EasyCache/Config.java
@@ -14,6 +14,11 @@ public class Config implements Serializable {
     final public static int N_CLIENT = N_L2 * 2; // number of clients
     final public static int N_ITEMS = 20; // number of items to be stored in the DB
 
+    final public static int RECOVERY_DELAY = 1500; //caches recover after these milliseconds
+
+    final public static int TIMEOUT_CACHE = 500; //cache goes in timeout after 500 ms
+
+    final public static int TIMEOUT_CLIENT = 1000; //client goes in timeout after 1000 ms
 
     // Method to print the current run configuration
     public static void printConfig(){
@@ -21,5 +26,8 @@ public class Config implements Serializable {
         LOGGER.info("N_L2: " + N_L2);
         LOGGER.info("N_CLIENT: " + N_CLIENT);
         LOGGER.info("N_ITEMS: " + N_ITEMS);
+        LOGGER.info("RECOVER_DELAY: " + RECOVERY_DELAY);
+        LOGGER.info("TIMEOUT_CACHE: " + TIMEOUT_CACHE);
+        LOGGER.info("TIMEOUT_CLIENT: " + TIMEOUT_CLIENT);
     }
 }
