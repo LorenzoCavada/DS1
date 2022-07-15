9bb3fd68d1365482e86c667b1a10749a2d559697
Alessandro Brighenti <alessandro.brighenti@studenti.unitn.it>
2022-07-15 11:53:43 +0200

    crash typ

---
+++ src/main/java/EasyCache/CrashType.java
@@ -0,0 +1,8 @@
+package EasyCache;
+
+// Enum used to identify the type of crashes
+public enum CrashType {
+    //TODO aggiungere altri tipi di crash
+    AFTER_L1_FORWARD,
+    AFTER_L2_FORWARD
+}
