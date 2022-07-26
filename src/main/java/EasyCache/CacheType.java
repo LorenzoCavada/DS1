48b6a010451f830258e636d85ec2a19a76b06267
Alessandro Brighenti <alessandro.brighenti@studenti.unitn.it>
2022-07-26 18:49:10 +0200

    javadoc cache type

--- src/main/java/EasyCache/CacheType.java
+++ src/main/java/EasyCache/CacheType.java
@@ -1,7 +1,16 @@
 package EasyCache;
 
-// Enum used to identify the type of the cache.
+/**
+ * Enum used to identify the type of the cache.
+ */
+
 public enum CacheType {
+    /**
+     * L1 cache
+     */
     L1,
+    /**
+     * L2 cache
+     */
     L2
 }
